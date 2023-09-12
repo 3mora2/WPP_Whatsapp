b4e9ff79dc09668e0f6e2812e0b7a98d05114170
Ammar <ammar.alkotb@gmail.com>
2023-09-12 20:00:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 long_description = open("README.md", encoding="utf-8").read()
 description = "WPP_Whatsapp aim of exporting functions from WhatsApp Web to the python, which can be used to support the creation of any interaction, such as customer service, media sending, intelligence recognition based on phrases artificial and many other things, use your imagination"
 
-version = "0.1.4"
+version = "0.1.5"
 
 
 setup(
