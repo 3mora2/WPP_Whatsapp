d9848668abcb531574eb62c736ed3758c8bd0bd8
3mora2 <66757189+3mora2@users.noreply.github.com>
2024-01-04 23:37:43 +0200

    Update SenderLayer.py

--- WPP_Whatsapp/api/layers/SenderLayer.py
+++ WPP_Whatsapp/api/layers/SenderLayer.py
@@ -6,14 +6,144 @@ from WPP_Whatsapp.api.layers.ListenerLayer import ListenerLayer
 
 class SenderLayer(ListenerLayer):
 
-    def sendLinkPreview(self, chatId, url, text=''):
+    def sendLinkPreview(self, chatId, url, text='', timeout=120):
         """
         * Automatically sends a link with the auto generated link preview. You can also add a custom message to be added.
         * Deprecated: please use {@link sendText}
         """
+        return self.ThreadsafeBrowser.run_threadsafe(self.sendLinkPreview_, chatId, url, text, timeout_=timeout)
+
+    def sendText(self, to, content, options=None, timeout=60):
+        """
+           Sends a text message to given chat
+           @category Chat
+           @param to chat to: xxxxx@us.c
+           @param content text message
+           @param options dict
+           @param timeout
+           @example
+           // Simple message
+           client.sendText('<number>@c.us', 'A simple message')
+           :return: dict
+        """
+        return self.ThreadsafeBrowser.run_threadsafe(self.sendText_, to, content, options, timeout_=timeout)
+
+    def sendMessageOptions(self, chat, content, options=None, timeout=60):
+        return self.ThreadsafeBrowser.run_threadsafe(self.sendMessageOptions_, chat, content, options, timeout_=timeout)
+
+    def sendImage(self, to, filePath, filename="", caption="", quotedMessageId=None, isViewOnce=None, timeout=120):
+        return self.ThreadsafeBrowser.run_threadsafe(
+            self.sendImage_, to, filePath, filename, caption,
+            quotedMessageId, isViewOnce, timeout_=timeout)
+
+    def reply(self, to, content, quotedMsg, timeout=60):
+        """
+            @param to chat to: xxxxx@us.c
+            @param content text message
+            @param quotedMsg: @param quotedMsg Message id to reply to.
+            @example
+           // Simple message
+           client.reply('<number>@c.us', 'A simple message', '<message-id>')
+        """
+        return self.ThreadsafeBrowser.run_threadsafe(
+            self.reply_, to, content, quotedMsg, timeout_=timeout)
+
+    def sendFile(self, to, pathOrBase64, nameOrOptions, caption, timeout=60):
+        return self.ThreadsafeBrowser.run_threadsafe(
+            self.sendFile_, to, pathOrBase64, nameOrOptions, caption, timeout_=timeout)
+
+    def sendContactVcard(self, to, contactsId, name, timeout=60):
+        """
+          /**
+           * Sends contact card to iven chat id
+           * @category Chat
+           * @param to Chat id
+           * @param contactsId Example: 0000@c.us | [000@c.us, 1111@c.us]
+           */
+        """
+        return self.ThreadsafeBrowser.run_threadsafe(
+            self.sendContactVcard_, to, contactsId, name, timeout_=timeout)
+
+    def forwardMessages(self, to, messages, skipMyMessages, timeout=60):
+        return self.ThreadsafeBrowser.run_threadsafe(
+            self.forwardMessages_, to, messages, skipMyMessages, timeout_=timeout)
+
+    def sendLocation(self, to, options, timeout=60):
+        return self.ThreadsafeBrowser.run_threadsafe(
+            self.sendLocation_, to, options, timeout_=timeout)
+
+    def sendSeen(self, chatId, timeout=60):
+        return self.ThreadsafeBrowser.run_threadsafe(
+            self.sendSeen_, chatId, timeout_=timeout)
+
+    def startTyping(self, to, duration=None, timeout=60):
+        return self.ThreadsafeBrowser.run_threadsafe(
+            self.startTyping_, to, duration, timeout_=timeout)
+
+    def stopTyping(self, to, timeout=60):
+        return self.ThreadsafeBrowser.run_threadsafe(
+            self.stopTyping_, to, timeout_=timeout)
+
+    def setOnlinePresence(self, online=True, timeout=60):
+        return self.ThreadsafeBrowser.run_threadsafe(
+            self.setOnlinePresence_, online, timeout_=timeout)
+
+    def sendListMessage(self, to, options, timeout=60):
+        """
+          /**
+           * Sends a list message
+           *
+           * ```typescript
+           *   // Example
+           *   client.sendListMessage('<number>@c.us', {
+           *     buttonText: 'Click here',
+           *     description: 'Choose one option',
+           *     sections: [
+           *       {
+           *         title: 'Section 1',
+           *         rows: [
+           *           {
+           *             rowId: 'my_custom_id',
+           *             title: 'Test 1',
+           *             description: 'Description 1',
+           *           },
+           *           {
+           *             rowId: '2',
+           *             title: 'Test 2',
+           *             description: 'Description 2',
+           *           },
+           *         ],
+           *       },
+           *     ],
+           *   });
+           * ```
+           *
+           * @category Chat
+           */
+        """
+        return self.ThreadsafeBrowser.run_threadsafe(
+            self.sendListMessage_, to, options, timeout_=timeout)
+
+    def setChatState(self, chatId, chatState, timeout=60):
+        """
+          /**
+           * Sets the chat state
+           * Deprecated in favor of Use startTyping or startRecording functions
+           * @category Chat
+           * @param chatState   Typing = 0, Recording = 1, Paused = 2
+           * @param chatId
+           * @deprecated Deprecated in favor of Use startTyping or startRecording functions
+           */
+        """
+        return self.ThreadsafeBrowser.run_threadsafe(
+            self.setChatState_, chatId, chatState, timeout_=timeout)
+
+    ####################################################### async ######################################################
+
+    async def sendLinkPreview_(self, chatId, url, text=''):
         message = text if url in text else f"{text}\n{url}"
         chatId = self.valid_chatId(chatId)
-        result = self.ThreadsafeBrowser.sync_page_evaluate("""({ chatId, message }) => {
+        result = await self.ThreadsafeBrowser.page_evaluate("""({ chatId, message }) => {
                     return WPP.chat.sendTextMessage(chatId, message, { linkPreview: true });
                     }""", {"chatId": chatId, "message": message})
         return result
@@ -31,50 +161,26 @@ class SenderLayer(ListenerLayer):
 
         return send_result
 
-    def sendText(self, to, content, options=None):
-        """
-           Sends a text message to given chat
-           @category Chat
-           @param to chat to: xxxxx@us.c
-           @param content text message
-           @param options dict
-           @example
-           // Simple message
-           client.sendText('<number>@c.us', 'A simple message')
-           :return: dict
-        """
+    async def sendMessageOptions_(self, chat, content, options=None):
         if not options:
             options = {}
-        to = self.valid_chatId(to)
-        send_result = self.ThreadsafeBrowser.sync_page_evaluate("""({ to, content, options }) =>
-                            WPP.chat.sendTextMessage(to, content, {
-                              ...options,
-                              waitForAck: true,
-                            })""", {"to": to, "content": content, "options": options})
-        self.logger.debug(f'{self.session}: Send Message {send_result=}')
-
-        return send_result
-
-    def sendMessageOptions(self, chat, content, options=None):
-        if not options:
-            options = {}
-        message_id = self.ThreadsafeBrowser.sync_page_evaluate("""({ chat, content, options }) => {
+        message_id = await self.ThreadsafeBrowser.page_evaluate("""({ chat, content, options }) => {
         return WAPI.sendMessageOptions(chat, content, options);
       }""", {"chat": chat, "content": content, "options": options})
-        result = self.ThreadsafeBrowser.sync_page_evaluate("""(messageId) => WAPI.getMessageById(messageId)""",
-                                                           message_id)
+        result = await self.ThreadsafeBrowser.page_evaluate("""(messageId) => WAPI.getMessageById(messageId)""",
+                                                            message_id)
         return result
 
-    def sendImage(self, to, filePath, filename="", caption="", quotedMessageId=None, isViewOnce=None):
+    async def sendImage_(self, to, filePath, filename="", caption="", quotedMessageId=None, isViewOnce=None):
         to = self.valid_chatId(to)
         if filePath and os.path.exists(filePath):
             _base64 = self.convert_to_base64(filePath)
             filename = os.path.basename(filePath) if not filename else filename
-            return self.sendImageFromBase64(to, _base64, filename, caption, quotedMessageId, isViewOnce)
+            return await self.sendImageFromBase64_(to, _base64, filename, caption, quotedMessageId, isViewOnce)
         else:
             raise Exception("Path Not Found")
 
-    def sendImageFromBase64(self, to, _base64, filename, caption, quotedMessageId, isViewOnce):
+    async def sendImageFromBase64_(self, to, _base64, filename, caption, quotedMessageId, isViewOnce):
         mime_type = self.base64MimeType(_base64)
         if not mime_type:
             raise Exception("Not valid mimeType")
@@ -83,7 +189,7 @@ class SenderLayer(ListenerLayer):
             raise Exception('Not an image, allowed formats png, jpeg and webp')
 
         # filename = filenameFromMimeType(filename, mimeType)
-        result = self.ThreadsafeBrowser.sync_page_evaluate("""async ({
+        result = await self.ThreadsafeBrowser.page_evaluate("""async ({
         to,
         base64,
         filename,
@@ -110,7 +216,7 @@ class SenderLayer(ListenerLayer):
              "isViewOnce": isViewOnce})
         return result
 
-    def reply(self, to, content, quotedMsg):
+    async def reply_(self, to, content, quotedMsg):
         """
             @param to chat to: xxxxx@us.c
             @param content text message
@@ -120,13 +226,13 @@ class SenderLayer(ListenerLayer):
            client.reply('<number>@c.us', 'A simple message', '<message-id>')
         """
         to = self.valid_chatId(to)
-        result = self.ThreadsafeBrowser.sync_page_evaluate("""({ to, content, quotedMsg }) => {
+        result = await self.ThreadsafeBrowser.page_evaluate("""({ to, content, quotedMsg }) => {
                                     return WPP.chat.sendTextMessage(to, content, { quotedMsg });
                                   }""", {"to": to, "content": content, "quotedMsg": quotedMsg})
-        # message = self.ThreadsafeBrowser.sync_page_evaluate()("(messageId) => WAPI.getMessageById(messageId)", result.get("id"))
+        # message = await self.ThreadsafeBrowser.page_evaluate()("(messageId) => WAPI.getMessageById(messageId)", result.get("id"))
         return result
 
-    def sendFile(self, to, pathOrBase64, nameOrOptions, caption, timeout_=60):
+    async def sendFile_(self, to, pathOrBase64, nameOrOptions, caption):
         to = self.valid_chatId(to)
         options = {"type": 'auto-detect'}
         if type(nameOrOptions) is str:
@@ -148,16 +254,16 @@ class SenderLayer(ListenerLayer):
         if not _base64:
             raise Exception("Empty or invalid file or base64")
 
-        return self.ThreadsafeBrowser.sync_page_evaluate("""async ({ to, base64, options }) => {
+        return await self.ThreadsafeBrowser.page_evaluate("""async ({ to, base64, options }) => {
         const result = await WPP.chat.sendFileMessage(to, base64, options);
         return {
           ack: result.ack,
           id: result.id,
           sendMsgResult: await result.sendMsgResult,
         };
-      }""", {"to": to, "base64": _base64, "options": options}, timeout_=timeout_)
+      }""", {"to": to, "base64": _base64, "options": options})
 
-    def sendContactVcard(self, to, contactsId, name):
+    async def sendContactVcard_(self, to, contactsId, name):
         """
           /**
            * Sends contact card to iven chat id
@@ -167,28 +273,28 @@ class SenderLayer(ListenerLayer):
            */
         """
         to = self.valid_chatId(to)
-        return self.ThreadsafeBrowser.sync_page_evaluate("""({ to, contactsId, name }) => {
+        return await self.ThreadsafeBrowser.page_evaluate("""({ to, contactsId, name }) => {
         return WPP.chat.sendVCardContactMessage(to, {
           id: contactsId,
           name: name,
         });
       }""", {"to": to, "contactsId": contactsId, "name": name})
 
-    def forwardMessages(self, to, messages, skipMyMessages):
+    async def forwardMessages_(self, to, messages, skipMyMessages):
         to = self.valid_chatId(to)
-        return self.ThreadsafeBrowser.sync_page_evaluate("""({ to, messages, skipMyMessages }) =>
+        return await self.ThreadsafeBrowser.page_evaluate("""({ to, messages, skipMyMessages }) =>
         WAPI.forwardMessages(to, messages, skipMyMessages)""",
-                                                         {"to": to, "messages": messages,
-                                                          "skipMyMessages": skipMyMessages})
+                                                          {"to": to, "messages": messages,
+                                                           "skipMyMessages": skipMyMessages})
 
-    def sendLocation(self, to, options):
+    async def sendLocation_(self, to, options):
         to = self.valid_chatId(to)
         options = {
             "lat": options.get("latitude"),
             "lng": options.get("longitude"),
             "title": options.get("title"),
         }
-        return self.ThreadsafeBrowser.sync_page_evaluate("""async ({ to, options }) => {
+        return await self.ThreadsafeBrowser.page_evaluate("""async ({ to, options }) => {
         const result = await WPP.chat.sendLocationMessage(to, options);
 
         return {
@@ -198,23 +304,24 @@ class SenderLayer(ListenerLayer):
         };
       }""", {"to": to, "options": options})
 
-    def sendSeen(self, chatId):
+    async def sendSeen_(self, chatId):
         chatId = self.valid_chatId(chatId)
-        return self.ThreadsafeBrowser.sync_page_evaluate("(chatId) => WPP.chat.markIsRead(chatId)", chatId)
+        return await self.ThreadsafeBrowser.page_evaluate("(chatId) => WPP.chat.markIsRead(chatId)", chatId)
 
-    def startTyping(self, to, duration=None):
+    async def startTyping_(self, to, duration=None):
         to = self.valid_chatId(to)
-        return self.ThreadsafeBrowser.sync_page_evaluate("({ to, duration }) => WPP.chat.markIsComposing(to, duration)",
-                                                         {"to": to, "duration": duration})
+        return await self.ThreadsafeBrowser.page_evaluate(
+            "({ to, duration }) => WPP.chat.markIsComposing(to, duration)",
+            {"to": to, "duration": duration})
 
-    def stopTyping(self, to):
+    async def stopTyping_(self, to):
         to = self.valid_chatId(to)
-        return self.ThreadsafeBrowser.sync_page_evaluate("(to) => WPP.chat.markIsPaused(to)", to)
+        return await self.ThreadsafeBrowser.page_evaluate("(to) => WPP.chat.markIsPaused(to)", to)
 
-    def setOnlinePresence(self, online=True):
-        return self.ThreadsafeBrowser.sync_page_evaluate("(online) => WPP.conn.markAvailable(online)", online)
+    async def setOnlinePresence_(self, online=True):
+        return await self.ThreadsafeBrowser.page_evaluate("(online) => WPP.conn.markAvailable(online)", online)
 
-    def sendListMessage(self, to, options):
+    async def sendListMessage_(self, to, options):
         to = self.valid_chatId(to)
         """
           /**
@@ -248,15 +355,15 @@ class SenderLayer(ListenerLayer):
            * @category Chat
            */
         """
-        sendResult = self.ThreadsafeBrowser.sync_page_evaluate(
+        sendResult = await self.ThreadsafeBrowser.page_evaluate(
             "({ to, options }) => WPP.chat.sendListMessage(to, options)",
             {"to": to, "options": options})
-        result = self.ThreadsafeBrowser.sync_page_evaluate("""async ({ messageId }) => {
+        result = await self.ThreadsafeBrowser.page_evaluate("""async ({ messageId }) => {
                         return JSON.parse(JSON.stringify(await WAPI.getMessageById(messageId)));
                       }""", sendResult.get("id"))
         return result
 
-    def setChatState(self, chatId, chatState):
+    async def setChatState_(self, chatId, chatState):
         """
           /**
            * Sets the chat state
@@ -268,7 +375,7 @@ class SenderLayer(ListenerLayer):
            */
         """
         chatId = self.valid_chatId(chatId)
-        return self.ThreadsafeBrowser.sync_page_evaluate("""({ chatState, chatId }) => {
+        return await self.ThreadsafeBrowser.page_evaluate("""({ chatState, chatId }) => {
                 WAPI.sendChatstate(chatState, chatId);
               }""", {"chatState": chatState, "chatId": chatId})
 
