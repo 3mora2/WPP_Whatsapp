9928264d94947b1df9a49385cc7656ddde57d745
3mora2 <66757189+3mora2@users.noreply.github.com>
2024-02-02 17:38:40 +0200

    Update send_text_message.py

--- examples/send_text_message.py
+++ examples/send_text_message.py
@@ -1,8 +1,9 @@
 from WPP_Whatsapp import Create
-import logging
 
-logger = logging.getLogger(name="WPP_Whatsapp")
-logger.setLevel(logging.DEBUG)
+# import logging
+
+# logger = logging.getLogger(name="WPP_Whatsapp")
+# logger.setLevel(logging.DEBUG)
 
 # start client with your session name
 your_session_name = "test"
